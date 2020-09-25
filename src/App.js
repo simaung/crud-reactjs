dbff3830cf0f5c962c96cfb3b1ebb56131c322a6
simaung <ddhr00@gmail.com>
2020-09-25 16:09:20 +0700

    install reactstrap dan fontawesome

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import JumbotronComponent from "./components/JumbotronComponent";
+import NavbarComponent from "./components/NavbarComponent";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    title: "Simaung Project",
+  };
+  render() {
+    return (
+      <div>
+        <NavbarComponent />
+        <JumbotronComponent title={this.state.title} />
+      </div>
+    );
+  }
 }
-
-export default App;
